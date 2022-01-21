d69c1836dcc7310174d311c77d828da13f1817a9
Rushikesh Susar <64800815+Rushikeshs2@users.noreply.github.com>
2022-01-21 20:10:02 +0530

    Update settings.py

--- coaching/settings.py
+++ coaching/settings.py
@@ -142,7 +142,7 @@ EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rspatil0103@gmail.com'
-EMAIL_HOST_PASSWORD = 'Rishi@2000'
+EMAIL_HOST_PASSWORD = '__password__'
 
 MESSAGE_TAGS = {
     messages.ERROR:'danger',
